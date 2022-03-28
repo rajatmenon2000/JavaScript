0438fc53292011c889aefb1376b0398c8eb10f31
Rajat Menon <22711531@student.uwa.edu.au>
2022-03-28 16:12:40 +0800

    Updating

--- lab3.js
+++ lab3.js
@@ -1,1 +1,10 @@
-var carName = "volvo";
+function changeText(){
+    document.getElementById("AWD").innerHTML = "CITS3401";
+    document.getElementById("DW").innerHTML = "CITS3403";
+    document.getElementById("CN").innerHTML = "CITS3002";
+    document.getElementById("OU").innerHTML = "SCIE1121";
+}
+
+const x = 10;
+const y = 20;
+let z = x + y;
