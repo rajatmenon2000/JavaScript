ac1ba48c144d1a048f6063f456e82b12b8f4aa12
Rajat Menon <22711531@student.uwa.edu.au>
2022-03-30 14:48:17 +0800

    Updating

--- functions.js
+++ functions.js
@@ -1,12 +1,31 @@
 function lcm(num1, num2) {
-
-    if (num1 < num2) {
-        high = num1 * num2;
-        for (let i; i < high; i++) {
-
+    if (num1 > num2) {
+        let big = num1;
+        let small = num2;
+        for (let i = big; i < big * small; i++) {
+            if (i % big == 0 && i % small == 0) {
+                console.log(i);
+                break;
+            }
+            else {
+                continue;
+            }
         }
     }
-    else{
-        
+    else if (num1 < num2) {
+        let small = num1;
+        let big = num2;
+        for (let i = big; i < big * small; i++) {
+            if (i % big == 0 && i % small == 0) {
+                console.log(i);
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+    }
+    else if (num1 == num2) {
+        console.log(num1);
     }
 }
