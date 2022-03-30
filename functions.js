b039b301f7dcdcd017cd40adf71d8b4bd8d7f75e
Rajat Menon <22711531@student.uwa.edu.au>
2022-03-30 12:43:21 +0800

    Updating

---
+++ functions.js
@@ -0,0 +1,12 @@
+function lcm(num1, num2) {
+
+    if (num1 < num2) {
+        high = num1 * num2;
+        for (let i; i < high; i++) {
+
+        }
+    }
+    else{
+        
+    }
+}
--- wordle.js
+++ wordle.js
@@ -9,7 +9,7 @@ function wordle(target, guess) {
         else if (target.includes(guess.charAt(0)) && target.charAt(0) != guess.charAt(0)) {
             first = "The first letter appears in the word, but in a different position";
         }
-        else{
+        else {
             first = "The first letter does not appear in the word";
         }
         if (target.charAt(1) == guess.charAt(1)) {
@@ -18,7 +18,7 @@ function wordle(target, guess) {
         else if (target.includes(guess.charAt(1)) && target.charAt(1) != guess.charAt(1)) {
             second = "the second letter appears in the word, but in a different position";
         }
-        else{
+        else {
             second = "the second letter does not appear in the word";
         }
         if (target.charAt(2) == guess.charAt(2)) {
@@ -27,7 +27,7 @@ function wordle(target, guess) {
         else if (target.includes(guess.charAt(2)) && target.charAt(2) != guess.charAt(2)) {
             third = "the third letter appears in the word, but in a different position";
         }
-        else{
+        else {
             third = "the third letter does not appear in the word";
         }
         if (target.charAt(3) == guess.charAt(3)) {
@@ -36,7 +36,7 @@ function wordle(target, guess) {
         else if (target.includes(guess.charAt(3)) && target.charAt(3) != guess.charAt(3)) {
             fourth = "the fourth letter appears in the word, but in a different position";
         }
-        else{
+        else {
             fourth = "the fourth letter does not appear in the word";
         }
         if (target.charAt(4) == guess.charAt(4)) {
@@ -45,13 +45,13 @@ function wordle(target, guess) {
         else if (target.includes(guess.charAt(4)) && target.charAt(4) != guess.charAt(4)) {
             fifth = "the fifth letter appears in the word, but in a different position";
         }
-        else{
+        else {
             fifth = "the fifth letter does not appear in the word";
         }
         console.log(first + ", " + second + ", " + third + ", " + fourth + ", " + fifth);
     }
 
-    else{
+    else {
         result = "Both arguments must be length of 5 letters!";
         console.log(result);
     }
